572ac662c8c42723ac4f0c4450eb0fd08b4665b8
Samuel Schwager <samuel.schwager@gmail.com>
2022-04-26 08:49:04 +0000

    add basic file io

--- main.clj
+++ main.clj
@@ -1,1 +1,13 @@
-(println "Hello World!")
+;; namespace alias, e.g io. namespace prefix must be used when referring to type.
+;;(ns advent-of-code-2021
+;;  (:require [clojure.java.io :as io]))
+
+;; import specific types, e.g. reader. can be used without namespace prefix.
+(ns advent-of-code-2021
+  (:require [clojure.java.io :refer [reader]]))
+
+(with-open [reader (reader "input.txt")]
+  (doseq [line (line-seq reader)]
+    (println line)))
+
+(println "Done!")
